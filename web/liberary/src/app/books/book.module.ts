1321e7b9d82465f60e1c8a2ff0dda3a715ac72a7
Yura Vusach <Yura_Vusach@epam.com>
2019-04-26 14:51:13 +0300

    search bar added

--- web/liberary/src/app/books/book.module.ts
+++ web/liberary/src/app/books/book.module.ts
@@ -8,10 +8,11 @@ import { BooksListComponent } from './books-list/books-list.component';
 import { BooksRoutingModule } from './books-routing.module';
 import { BooksPageComponent } from '../book-page/book-page.component';
 import { BooksPaginationComponent } from './book-pagination/books-pagination.component';
+import { BooksSearchComponent } from './book-search/book-search.component';
 
 @NgModule({
   imports:      [ CommonModule, SharedModule , FormsModule, BooksRoutingModule ],
-  declarations: [ BooksComponent , BooksListComponent, BooksPaginationComponent ],
-  exports: [ BooksComponent, BooksListComponent, BooksPaginationComponent ]
+  declarations: [ BooksComponent , BooksListComponent, BooksPaginationComponent, BooksSearchComponent ],
+  exports: [ BooksComponent, BooksListComponent, BooksPaginationComponent, BooksSearchComponent ]
 })
 export class BooksModule { }
